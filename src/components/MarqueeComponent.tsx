5edad3dee377af1450dc0ca3d47af30f71d159b8
NishantGupt786 <nishantgupta2325@gmail.com>
2024-09-05 17:48:07 +0530

    font fixes

--- src/components/MarqueeComponent.tsx
+++ src/components/MarqueeComponent.tsx
@@ -1,35 +1,47 @@
 "use client";
 
-import React from 'react';
-import Marquee from 'react-fast-marquee';
+import React from "react";
+import Marquee from "react-fast-marquee";
 
 const MarqueeComponent: React.FC = () => {
   return (
-    <div style={{ width: '100%', overflow: 'hidden' }}>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-        <div style={{ backgroundColor: '#D9D9D9', width: '50%', height: '10px' }}></div>
-        <div style={{ backgroundColor: '#D9D9D9', width: '50%', height: '10px' }}></div>
+    <div className="w-full overflow-hidden">
+      <div className="flex flex-row items-center w-full">
+        <div className="bg-gray-300 w-1/2 h-2"></div>
+        <div className="bg-gray-300 w-1/2 h-2"></div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-        <div style={{ backgroundColor: 'black', width: '50%', height: '10px' }}></div>
-        <div style={{ backgroundColor: 'black', width: '50%', height: '10px' }}></div>
+      <div className="flex flex-row items-center w-full">
+        <div className="bg-black w-1/2 h-2"></div>
+        <div className="bg-black w-1/2 h-2"></div>
       </div>
-      <Marquee speed={80} gradient={false} style={{ backgroundColor: "#D9D9D9" ,border: '1px solid #ccc', padding: '10px' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
-          <div style={{ width: '100%', textAlign: 'center', padding: '30px', wordSpacing: '20px', fontWeight: "700" , fontSize: "64px", fontFamily: "IBM plex Mono" }}>
-            CLUEMINATI AND COOKOFF DO REGISTER  CLUEMINATI AND COOKOFF DO REGISTER.  CLUEMINATI AND COOKOFF DO REGISTER.  CLUEMINATI AND COOKOFF DO REGISTER.  CLUEMINATI AND COOKOFF DO REGISTER.
+      <Marquee
+        speed={80}
+        gradient={false}
+        className="bg-gray-300 border border-gray-300 p-4 md:p-14"
+      >
+        <div className="flex flex-col items-center w-full">
+          <div
+            className="w-full text-center p-7.5 space-x-5 font-bold text-4xl md:text-6xl font-mono"
+            style={{
+              fontFamily: "Anton Regular",
+              fontStyle: "normal",
+              fontWeight: 50,
+            }}
+          >
+            CLUEMINATI AND COOKOFF DO REGISTER CLUEMINATI AND COOKOFF DO
+            REGISTER. CLUEMINATI AND COOKOFF DO REGISTER. CLUEMINATI AND COOKOFF
+            DO REGISTER. CLUEMINATI AND COOKOFF DO REGISTER.
           </div>
         </div>
       </Marquee>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-        <div style={{ backgroundColor: 'black', width: '50%', height: '10px' }}></div>
-        <div style={{ backgroundColor: 'black', width: '50%', height: '10px' }}></div>
+      <div className="flex flex-row items-center w-full">
+        <div className="bg-black w-1/2 h-2"></div>
+        <div className="bg-black w-1/2 h-2"></div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', width: '100%' }}>
-        <div style={{ backgroundColor: 'white', width: '50%', height: '10px' }}></div>
-        <div style={{ backgroundColor: 'white', width: '50%', height: '10px' }}></div>
+      <div className="flex flex-row items-center w-full">
+        <div className="bg-white w-1/2 h-2"></div>
+        <div className="bg-white w-1/2 h-2"></div>
       </div>
-      
     </div>
   );
 };
--- src/components/sections/navbar.tsx
+++ src/components/sections/navbar.tsx
@@ -1,67 +1,81 @@
 import Link from "next/link";
 import Image from "next/image";
-import mmLogo from "@/assets/mmLogo.png"
+import mmLogo from "@/assets/mmLogo.png";
 
 const Navbar = () => {
+  return (
+    <div className="h-screen bg-black bg-[url('/magicpattern-grid-pattern.svg')]">
+      <div className="flex justify-between items-center max-w-full relative px-6 py-4">
+        <Link
+          href="/"
+          className="h-[74px] ml-2 sm:ml-4 md:ml-6 mt-3 pt-0 flex-shrink-0"
+        >
+          <Image src="/vector.svg" alt="Codechef_Logo" width={74} height={15} />
+        </Link>
 
-    return (
-        <div className="h-screen bg-black bg-[url('/magicpattern-grid-pattern.svg')]">
-            <div className="flex justify-between items-center max-w-full relative px-6 py-4">
-
-                <Link
-                    href="/"
-                    className="h-[74px] ml-2 sm:ml-4 md:ml-6 mt-3 pt-0 flex-shrink-0"
-                >
-                    <Image src="/vector.svg" alt="Codechef_Logo" width={74} height={15} />
-                </Link>
-
-
-                <ul className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-12 text-white font-bold anton-regular">
-                    <Link
-                        href="/"
-                        className="flexCenter mt-5 text-xl sm:text-2xl md:text-3xl"
-                    >
-                        COOKOFF
-                    </Link>
-                    <Link
-                        href="/"
-                        className="flexCenter mt-5 mr-2 sm:mr-4 md:mr-5 text-xl sm:text-2xl md:text-3xl"
-                    >
-                        CLUEMINATI
-                    </Link>
-                </ul>
-            </div>
-            <div style={{
-                fontFamily: "AudioWide-Regular",
-                fontStyle: "normal",
-                fontWeight: 100,
-            }} className="md:text-7xl text-5xl audiowide-regular w-screen flex justify-center text-white text-center">
-                Gravitas'24
-            </div>
-            <div style={{
-                fontFamily: "AudioWide-Regular",
-                fontStyle: "normal",
-                fontWeight: 100,
-            }} className="raleway-dots-regular w-full flex justify-center text-white text-center">
-                CO-HOSTED BY 
-            </div>
-            <div style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "-25px"
-            }}>
-            <Image src={mmLogo} alt="muscle mind logo" height={80} width={80}/>
-            
-            </div>
-            <div style={{
-                fontFamily: "AudioWide-Regular",
-                fontStyle: "normal",
-                fontWeight: 100,
-            }} className="raleway-dots-regular w-full flex justify-center text-white text-center">
-                Musclemind
-            </div>
-        </div> 
-    );
+        <ul
+          className="flex gap-4 sm:gap-6 md:gap-8 lg:gap-12 text-white font-semibold"
+          style={{
+            fontFamily: "Anton Regular",
+            fontStyle: "normal",
+            fontWeight: 50,
+          }}
+        >
+          <Link
+            href="/"
+            className="flexCenter mt-5 text-xl sm:text-2xl md:text-3xl"
+          >
+            COOKOFF
+          </Link>
+          <Link
+            href="/"
+            className="flexCenter mt-5 mr-2 sm:mr-4 md:mr-5 text-xl sm:text-2xl md:text-3xl"
+          >
+            CLUEMINATI
+          </Link>
+        </ul>
+      </div>
+      <div
+        style={{
+          fontFamily: "AudioWide-Regular",
+          fontStyle: "normal",
+          fontWeight: 100,
+        }}
+        className="md:text-7xl text-5xl audiowide-regular w-screen flex justify-center text-white text-center"
+      >
+        Gravitas'24
+      </div>
+      <div
+        style={{
+          fontFamily: "AudioWide-Regular",
+          fontStyle: "normal",
+          fontWeight: 100,
+        }}
+        className="raleway-dots-regular w-full flex justify-center text-white text-center"
+      >
+        CO-HOSTED BY
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "-25px",
+        }}
+      >
+        <Image src={mmLogo} alt="muscle mind logo" height={80} width={80} />
+      </div>
+      <div
+        style={{
+          fontFamily: "AudioWide-Regular",
+          fontStyle: "normal",
+          fontWeight: 100,
+        }}
+        className="raleway-dots-regular w-full flex justify-center text-white text-center"
+      >
+        Musclemind
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
